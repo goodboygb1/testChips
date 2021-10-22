ea13fb8e87b6646ed51d6fbd3d0ac58a6cefd97c
Poom Wutthidech <wutthidech@hotmail.com>
2021-10-22 23:11:28 +0700

    test chips

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'testChip' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for testChip
+pod 'MaterialComponents/TextControls+FilledTextAreas'
+pod 'MaterialComponents/TextControls+FilledTextFields'
+pod 'MaterialComponents/TextControls+OutlinedTextAreas'
+pod 'MaterialComponents/TextControls+OutlinedTextFields'
+pod 'MaterialComponents'
+pod 'MaterialComponents/TextFields'
+pod 'MaterialComponents/Chips'
+
+end
---
+++ testChip/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  testChip
+//
+//  Created by PMJs on 22/10/2564 BE.
+//
+
+import UIKit
+import MaterialComponents.MaterialChips
+import MaterialComponents.MaterialTextFields
+import MaterialComponents.MaterialContainerScheme
+
+
+class ViewController: UIViewController, UITextFieldDelegate, MDCChipFieldDelegate, UITextFieldDelegate{
+
+    var containerScheming = MDCContainerScheme()
+    var chipField: MDCChipField!
+    var textField: MDCOutlinedTextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupChipField()
+        setupTextField()
+    }
+    
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        reArrangeView()
+    }
+    
+
+    func chipFieldHeightDidChange(_ chipField: MDCChipField) {
+        view.layoutIfNeeded()
+    }
+
+    func chipField(_ chipField: MDCChipField, didRemoveChip chip: MDCChipView) {
+        view.layoutSubviews()
+        reArrangeView()
+    }
+
+    func setupChipField() {
+        chipField = MDCChipField()
+        chipField.frame = .zero
+        chipField.delegate = self
+        chipField.textField.placeholderLabel.text = "This is a chip field."
+        chipField.backgroundColor = containerScheming.colorScheme.surfaceColor
+        chipField.showChipsDeleteButton = true
+        self.view.addSubview(chipField)
+    }
+
+    func setupTextField() {
+        textField = MDCOutlinedTextField(frame: .zero)
+        textField.label.text = "Phone number"
+        textField.placeholder = "555-555-5555"
+        textField.sizeToFit()
+        textField.delegate = self
+        view.addSubview(textField)
+    }
+
+    func reArrangeView() {
+        var frame = view.bounds
+        frame = frame.inset(by: view.safeAreaInsets)
+        frame.size = chipField.sizeThatFits(frame.size)
+        chipField.frame = frame
+
+        let frame2 = CGRect(x: frame.origin.x+20, y: frame.origin.y+frame.size.height+20, width: frame.width-20, height: frame.height)
+        textField.frame = frame2
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let chipView = MDCChipView()
+        chipView.titleLabel.text = "Tap me"
+        chipView.setTitleColor(UIColor.red, for: .selected)
+        chipView.sizeToFit()
+        textField.addSubview(chipView)
+    }
+}
